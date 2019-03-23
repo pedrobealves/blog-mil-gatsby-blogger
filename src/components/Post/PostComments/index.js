e1f6fde6637a0c70319be15995949470a2e8b08a
PedroTreck <pedrobealves@gmail.com>
2019-03-23 16:00:30 -0300

    Feat: lazy load facebook comments

--- src/components/Post/PostComments/index.js
+++ src/components/Post/PostComments/index.js
@@ -1,17 +1,42 @@
-import React from 'react'
-import { FacebookProvider, Comments } from 'react-facebook'
-import configFacebook from '../../../utils/facebookConfig'
+import React, { Suspense, useState } from 'react'
+const FacebookComment = React.lazy(() => import('./FacebookComment'))
 
 const PostComments = ({ pagePath }) => {
+  const [showComments, setShowComments] = useState(false)
+
   return (
     <div className="post-comments card card--lg">
       <header className="post-commments__header card__header">
         <h4>Comments (18)</h4>
       </header>
       <div className="post-comments__content card__content">
-        <FacebookProvider wait appId={configFacebook.apiId}>
-          <Comments href={'https://www.blogmil.net/' + pagePath} />
-        </FacebookProvider>
+        {showComments ? (
+          <Suspense
+            fallback={
+              <button
+                type="button"
+                className="btn btn-secondary btn-lg btn-block"
+              >
+                <span
+                  className="spinner-grow spinner-grow-sm"
+                  role="status"
+                  aria-hidden="true"
+                />
+                <span className="sr-only">Carregando...</span>
+              </button>
+            }
+          >
+            <FacebookComment pagePath={pagePath} />
+          </Suspense>
+        ) : (
+          <button
+            onClick={() => setShowComments(true)}
+            type="button"
+            className="btn btn-secondary btn-lg btn-block"
+          >
+            Mostrar Comentários
+          </button>
+        )}
       </div>
     </div>
   )
