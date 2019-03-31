35fd43d4f534bb01e9e148d3e1c3e3a433c4de95
PedroTreck <pedrobealves@gmail.com>
2019-03-31 15:46:50 -0300

    Issue: dark mode

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,12 +4,11 @@ import 'typeface-montserrat'
 import { ThemeProvider } from './src/context/ThemeContext'
 
 require('bootstrap/dist/css/bootstrap.css')
+require('./src/styles/assets/scss/style-basketball.scss')
 
 const lsDark = JSON.parse(localStorage.getItem('dark'))
 
-lsDark
-  ? require('./src/styles/assets/scss/style-basketball-dark.scss')
-  : require('./src/styles/assets/scss/style-basketball.scss')
+lsDark && require('./src/styles/assets/scss/style-basketball-dark.scss')
 
 export const wrapRootElement = ({ element }) => (
   <ThemeProvider>{element}</ThemeProvider>
