349af82fd1678284554cda32275fd4f264d9317b
trecky <pedrobealves@gmail.com>
2019-10-20 16:00:45 -0300

    Pogress bar

--- src/components/Post/PostBody/index.js
+++ src/components/Post/PostBody/index.js
@@ -1,40 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import PostCategory from './PostCategory'
 import PostHeader from './PostHeader'
 import PostContent from './PostContent'
 import PostFooter from './PostFooter'
-import Modal from './Modal'
-import useModal from './useModal'
 
 const PostBody = ({ body: { frontmatter, html }, content, labels }) => {
-  const { isShowing, toggle } = useModal()
-
-  const [lastArticle, setLastArticle] = useState()
-  const [scroll, setScroll] = useState()
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setLastArticle(localStorage.getItem('lastArticle'))
-      setScroll(localStorage.getItem('scroll'))
-    }
-
-    lastArticle === frontmatter.slug && toggle()
-  }, [lastArticle, scroll])
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      window.onscroll = function() {
-        if (window.scrollY > 2000) {
-          localStorage.setItem('lastArticle', frontmatter.slug)
-          localStorage.setItem('scroll', window.scrollY)
-        }
-      }
-    }
-  }, [])
 
   return (
     <div className="card__content">
-      <Modal isShowing={isShowing} hide={toggle} scroll={scroll} />
       {labels && <PostCategory label={labels[0]} />}
       <PostHeader header={frontmatter} />
       <PostContent content={content} />
---
+++ src/components/Post/ReadingProgress/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const ProgressBar = styled.div`
+    position:  fixed;
+    background-color: #ff6711;
+    width:  100%;
+    height:  4px;
+    z-index:  3;
+`;
+
+export {
+    ProgressBar
+}
