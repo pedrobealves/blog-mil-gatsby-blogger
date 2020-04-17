885d3716f66042059d74dd50847aa842916e73ac
Pedro Bernardi <pedrobealves@gmail.com>
2020-04-17 16:55:49 +0000

    Update plugin blogger

--- plugins/gatsby-source-blogger/gatsby-node.js
+++ plugins/gatsby-source-blogger/gatsby-node.js
@@ -1,29 +1,19 @@
-'use strict'
-
-const { google } = require('googleapis')
-
-const unified = require('unified')
-
-const parse = require('rehype-parse')
-
-const rehype2remark = require('rehype-remark')
-
-const stringify = require('remark-stringify')
-
+const { google } = require("googleapis");
+const unified = require("unified");
+const parse = require("rehype-parse");
+const rehype2remark = require("rehype-remark");
+const stringify = require("remark-stringify");
 const { createRemoteFileNode } = require(`gatsby-source-filesystem`)
+const crypto = require("crypto");
 
-const crypto = require('crypto')
+const typePrefix = "blogger__";
 
-const typePrefix = 'blogger__'
 const refactoredEntityTypes = {
   post: `${typePrefix}POST`,
-  page: `${typePrefix}PAGE`,
-}
+  page: `${typePrefix}PAGE`
+};
 
 const handleResponse = str => {
-  // const rex = /<img\ssrc=(?:(?:'([^']*)')|(?:"([^"]*)")|([^\s]*))/i
-  // const res = str.match(re)
-  // const src = res[1] || res[2] || res[3]
   const re = /<img[^>]+src="?([^"\s]+)"?[^>]*\/>/g
   const results = re.exec(str)
   let img = ''
@@ -31,29 +21,38 @@ const handleResponse = str => {
   return img
 }
 
-exports.sourceNodes = async (
-  { actions, createNodeId, store, cache },
-  { apiKey, blogId }
-) => {
-  const { createNode, setPluginStatus } = actions
+exports.sourceNodes = async ({ actions, createNodeId, store, cache }, { apiKey, blogId }) => {
+  const { createNode, setPluginStatus } = actions;
+
   const blogger = google.blogger({
-    version: 'v3',
-    auth: apiKey,
-  })
-  let postResult
+    version: "v3",
+    auth: apiKey
+  });
+
+  let postResult;
+  let posts = [];
 
   try {
-    postResult = await blogger.posts.list({
+    let params = {
       blogId: blogId,
-      maxResults: 500,
-    })
+      maxResults: 500
+    };
+
+    do {
+      postResult = await blogger.posts.list(params);
+      if (postResult.data.nextPageToken) {
+        params = { ...params, pageToken: postResult.data.nextPageToken };
+      }
+      if (postResult.data.items) {
+        posts.push(...postResult.data.items);
+      }
+    } while (postResult.data.nextPageToken);
   } catch (err) {
-    console.log('Error fetching posts', err)
+    console.log("Error fetching posts", err);
   }
 
-  // const rePost = /^https?:\/\/(?:[^/]+)\/\d{4}\/\d{2}\/([^/][^.]+)\.html$/
+  //const rePost = /^https?:\/\/(?:[^/]+)\/\d{4}\/\d{2}\/([^/][^.]+)\.html$/;
   const rePost = /^https?:\/\/(?:[^/]+)(\/\d{4}\/\d{2}\/[^/][^.]+\.html)$/
-  const posts = postResult.data.items
 
   if (posts) {
     posts.forEach(post => {
@@ -68,64 +67,69 @@ exports.sourceNodes = async (
           if (fileNode) {
             post.cover___NODE = fileNode.id
           }
-
-          unified()
-            .use(parse)
-            .use(rehype2remark)
-            .use(stringify)
-            .process(post.content, function(err, md) {
-              if (err) console.log(err)
-              const segments = rePost.exec(post.url)
-              const gatsbyPost = Object.assign(
-                {
-                  slug: segments[1],
-                },
-                post,
-                {
-                  id: createNodeId(post.id),
-                  parent: null,
-                  children: [],
-                  internal: {
-                    type: refactoredEntityTypes.post,
-                    mediaType: `text/markdown`,
-                    content: `---
-title: '${post.title.replace("'", "''")}'
-date: '${post.published}'
-slug: '${segments[1]}'
+      unified()
+        .use(parse)
+        .use(rehype2remark)
+        .use(stringify)
+        .process(post.content, function(err, md) {
+          if (err) console.log(err);
+          const segments = rePost.exec(post.url);
+          const gatsbyPost = Object.assign({ slug: segments[1], labels: post.labels }, post, {
+            id: createNodeId(post.id),
+            parent: null,
+            children: [],
+            internal: {
+              type: refactoredEntityTypes.post,
+              mediaType: `text/markdown`,
+              content: `---
+title: >-
+  ${post.title}
+date: ${post.published}
+slug: ${segments[1]}
 labels: '${post.labels}'
 type: '${refactoredEntityTypes.post}'
 ---
 
 ${md}`,
-                    contentDigest: crypto
-                      .createHash(`md5`)
-                      .update(JSON.stringify(post))
-                      .digest(`hex`),
-                  },
-                }
-              )
-              createNode(gatsbyPost)
-            })
-        })
+              contentDigest: crypto
+                .createHash(`md5`)
+                .update(JSON.stringify(post))
+                .digest(`hex`)
+            }
+          });
+
+          createNode(gatsbyPost);
+        });
+                })
         .catch(e => {
           console.error('[gatsby-transformer-image]', 'Error', e.message)
         })
-    })
+    });
   }
 
-  let pageResult
+  let pageResult;
+  let pages = [];
 
   try {
-    pageResult = await blogger.pages.list({
+    let params = {
       blogId: blogId,
-      maxResults: 500,
-    })
+      maxResults: 500
+    };
+
+    do {
+      pageResult = await blogger.pages.list(params);
+      if (pageResult.data.nextPageToken) {
+        params = { ...params, pageToken: pageResult.data.nextPageToken };
+      }
+      if (pageResult.data.items) {
+        pages.push(...pageResult.data.items);
+      }
+    } while (pageResult.data.nextPageToken);
   } catch (err) {
-    console.log('Error fetching pages', err)
+    console.log("Error fetching pages", err);
   }
 
-  const rePage = /^https?:\/\/(?:[^/]+)\/p\/([^/][^.]+)\.html$/
-  const pages = pageResult.data.items
+  const rePage = /^https?:\/\/(?:[^/]+)\/p\/([^/][^.]+)\.html$/;
 
   if (pages) {
     pages.forEach(page => {
@@ -134,41 +138,37 @@ ${md}`,
         .use(rehype2remark)
         .use(stringify)
         .process(page.content, function(err, md) {
-          if (err) console.log(err)
-          const segments = rePage.exec(page.url)
-          const gatsbyPage = Object.assign(
-            {
-              slug: '/' + segments[1],
-            },
-            page,
-            {
-              id: createNodeId(page.id),
-              parent: null,
-              children: [],
-              internal: {
-                type: refactoredEntityTypes.page,
-                mediaType: `text/markdown`,
-                content: `---
-  title: '${page.title.replace("'", "''")}'
-  date: '${page.published}'
-  slug: '/${segments[1]}'
-  labels: ''
-  templateKey: 'page'
+          if (err) console.log(err);
+          const segments = rePage.exec(page.url);
+          const gatsbyPage = Object.assign({ slug: segments[1] }, page, {
+            id: createNodeId(page.id),
+            parent: null,
+            children: [],
+            internal: {
+              type: refactoredEntityTypes.page,
+              mediaType: `text/markdown`,
+              content: `---
+  title: >-
+    ${page.title}
+  date: ${page.published}
+  slug: ${segments[1]}
+  type: '${refactoredEntityTypes.page}'
   `,
-                contentDigest: crypto
-                  .createHash(`md5`)
-                  .update(JSON.stringify(page))
-                  .digest(`hex`),
-              },
+              contentDigest: crypto
+                .createHash(`md5`)
+                .update(JSON.stringify(page))
+                .digest(`hex`)
             }
-          )
-          createNode(gatsbyPage)
-        })
+          });
+
+          createNode(gatsbyPage);
+        });
+
       setPluginStatus({
         status: {
-          lastFetched: Date.now(),
-        },
-      })
-    })
+          lastFetched: Date.now()
+        }
+      });
+    });
   }
-}
+};
